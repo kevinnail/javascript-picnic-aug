767682943f1ba46797188c87e8a16553ba97e194
kevinnail <fumalicious@gmail.com>
2022-08-11 18:58:49 -0700

    add counter to all 5 items

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const items = document.getElementById('items');
+let numberOfItems = 0;
 const soda = document.getElementById('soda');
 
 soda.addEventListener('click', () => {
     console.log('clicking');
     soda.classList.toggle('picked');
+    if (soda.classList.contains('picked')) {
+        numberOfItems = numberOfItems +1;
+    } else {
+        numberOfItems = numberOfItems -1;
+    }
+    
+    items.textContent = numberOfItems;
 });
 
 const cheese = document.getElementById('cheese');
@@ -10,6 +19,12 @@ const cheese = document.getElementById('cheese');
 cheese.addEventListener('click', () =>{
     console.log('clicking');
     cheese.classList.toggle('picked');
+    if (cheese.classList.contains('picked')) {
+        numberOfItems = numberOfItems + 1; 
+    } else {
+        numberOfItems = numberOfItems - 1;
+    }
+    items.textContent = numberOfItems;
 });
 
 
@@ -18,6 +33,12 @@ const grapes = document.getElementById('grapes');
 grapes.addEventListener('click', () => {
     console.log('clicking');
     grapes.classList.toggle('picked');
+    if (grapes.classList.contains('picked')){
+        numberOfItems=numberOfItems + 1;
+    } else { 
+        numberOfItems=numberOfItems - 1;
+    }
+    items.textContent = numberOfItems;
 });
 
 const bread = document.getElementById('bread');
@@ -25,6 +46,12 @@ const bread = document.getElementById('bread');
 bread.addEventListener('click', () => {
     console.log('clicking');
     bread.classList.toggle('picked');
+    if (bread.classList.contains('picked')){
+        numberOfItems=numberOfItems+1;
+    } else {
+        numberOfItems=numberOfItems-1;
+    }
+    items.textContent = numberOfItems;
 });
 
 const watermelon = document.getElementById('watermelon');
@@ -32,4 +59,15 @@ const watermelon = document.getElementById('watermelon');
 watermelon.addEventListener('click', () => {
     console.log('clicking');
     watermelon.classList.toggle('picked');
+    if (watermelon.classList.contains('picked')){
+        numberOfItems=numberOfItems+1;
+    } else {
+        numberOfItems=numberOfItems-1;
+    }
+    items.textContent = numberOfItems;
+    
 });
+
+
+
+
