62fe68ef2425a85c16f8b8e7a36e5b1911449495
kevinnail <fumalicious@gmail.com>
2022-08-11 18:13:06 -0700

    add picked class to stuff

---
+++ app.js
@@ -0,0 +1,35 @@
+const soda = document.getElementById('soda');
+
+soda.addEventListener('click', () => {
+    console.log('clicking');
+    soda.classList.add('picked');
+});
+
+const cheese = document.getElementById('cheese');
+
+cheese.addEventListener('click', () =>{
+    console.log('clicking');
+    cheese.classList.add('picked');
+});
+
+
+const grapes = document.getElementById('grapes');
+
+grapes.addEventListener('click', () => {
+    console.log('clicking');
+    grapes.classList.add('picked');
+});
+
+const bread = document.getElementById('bread');
+
+bread.addEventListener('click', () => {
+    console.log('clicking');
+    bread.classList.add('picked');
+});
+
+const watermelon = document.getElementById('watermelon');
+
+watermelon.addEventListener('click', () => {
+    console.log('clicking');
+    watermelon.classList.add('picked');
+});
